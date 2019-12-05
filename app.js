aeb7cc1073703aa43e42afb94a3f4ead6f02c6a0
amit <amitd091@outlook.com>
2019-12-05 12:58:48 +0545

    View Template added to the Project

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -32,10 +32,9 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('partials/error');
 });
 
 module.exports = app;
